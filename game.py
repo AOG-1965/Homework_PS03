d938c326b5f5f8483d4853d565a88b0801400970
Андронов Олег <gennadych-65@yandex.ru>
2025-03-09 11:37:58 +0300

    add Translator and def translator.translate of word_definition

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-
+from googletrans import Translator
 
 # Создаём функцию, которая будет получать информацию
 def get_english_words():
@@ -23,20 +23,28 @@ def get_english_words():
     except:
         print("Произошла ошибка")
 
+# Создаём функцию игры:
+# рандомно выбранное определение слова переводится на русский язык
+# пользователь предлагает свой вариант слова на английском языке, соответствующее определению.
+# ответ пользователя оценивается на соответствие загаданному слову
 
-# Создаём функцию, которая будет делать саму игру
 def word_game():
     print("Добро пожаловать в игру")
+    translator = Translator()  # Создаём объект переводчика
+
     while True:
         # Создаём функцию, чтобы использовать результат функции-словаря
         word_dict = get_english_words()
         word = word_dict.get("english_words")
         word_definition = word_dict.get("word_definition")
 
+        # Переводим определение на русский язык
+        translated_definition = translator.translate(word_definition, src='en', dest='ru').text
+
         # Начинаем игру
-        print(f"Значение слова - {word_definition}")
+        print(f"Значение слова - {translated_definition}")
         user = input("Что это за слово? ")
-        if user == word:
+        if user == word:  # Слово вводится на английском языке
             print("Все верно!")
         else:
             print(f"Ответ неверный, было загадано это слово - {word}")
@@ -47,5 +55,6 @@ def word_game():
             print("Спасибо за игру!")
             break
 
-
 word_game()
+
+# УГАДАТЬ СЛОВО НЕРЕАЛЬНО ИЗ-ЗА НЕТОЧНОСТИ ПЕРЕВОДА ЕГО ОПРЕДЕЛЕНИЯ
